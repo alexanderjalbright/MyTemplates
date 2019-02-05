30a6f5e61957e6096df21934e39a38ead0d8f10b
Alex Albright <alexanderjalbright@gmail.com>
2019-02-05 16:11:47 -0500

    Intial Commit

---
+++ MyTemplates/Tools.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTemplates
+{
+    class Tools
+    {
+        public string ReplaceUserStringWithActualOption(string whichKind, List<string> list)
+        {
+            // since I am a terrible speller this helps find a close match of variety
+            int matchPoint = 0;
+
+            for (int i = 1; i <= list.Count; i++)
+            {
+                string temp = whichKind.Remove(i).ToLower();
+                int matches = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (temp.Equals(list[j].Remove(i).ToLower()))
+                    {
+                        matches++;
+                    }
+                }
+
+                if (matches == 1)
+                {
+                    matchPoint = i;
+                    foreach (string item in list)
+                    {
+                        if (whichKind.Remove(matchPoint).ToLower().Equals(item.Remove(matchPoint).ToLower()))
+                        {
+                            whichKind = item;
+                        }
+                    }
+                    break;
+                }
+            }
+            return whichKind;
+        }
+
+        static int NumberCheck()
+        {
+            bool error;
+            int definitelyNumber;
+            do
+            {
+                error = false;
+                string checkIsNumber = Console.ReadLine();
+                if (!int.TryParse(checkIsNumber, out definitelyNumber))
+                {
+                    Console.WriteLine("You must enter a number.");
+                    Console.WriteLine("Try again.");
+                    error = true;
+                }
+            } while (error);
+
+            return definitelyNumber;
+        }
+    }
+}
